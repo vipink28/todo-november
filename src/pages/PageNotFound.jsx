c77d3aa06d71bc5040eac7c22873adbe734e2bdd
vipink28 <vipinkalyan28@gmail.com>
2023-12-11 10:35:01 +0530

    routing created

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageNotFound(props) {
+    return (
+        <div>
+            Error 404: Page not found
+        </div>
+    );
+}
+
+export default PageNotFound;
