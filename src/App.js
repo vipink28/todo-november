f0083e2a7cd8aa8d6b334ec0012393b23ae87459
vipink28 <vipinkalyan28@gmail.com>
2023-12-12 11:59:02 +0530

    user registered

--- src/App.js
+++ src/App.js
@@ -15,20 +15,6 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" />}></Route>
-        <Route path='/' element={<Home />}>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/register' element={<Register />}></Route>
-        </Route>
-        <Route path='/about' element={<About />}></Route>
-        <Route path='/create-task' element={<CreateTask />}></Route>
-        <Route path='/task-list' element={<TaskList />}></Route>
-        <Route path='/profile' element={<Profile />}></Route>
-        <Route path='*' element={<PageNotFound />}></Route>
-      </Routes>
-
       <Routes>
         <Route path='/' element={<Navigate to="/login" />}></Route>
         <Route path='/' element={<Home />}>
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register(props) {
+    const [formData, setFormData] = useState(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+        const config = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+        const response = await fetch(`http://localhost:5000/users`, config);
+        if (response.status === 201) {
+            alert("user Registered");
+        } else {
+            alert("something went wrong");
+        }
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <form>
+            <div className='mb-3'>
+                <label className='form-label'>Name</label>
+                <input type="text" className='form-control' name='name' onChange={handleChange} />
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'>Email</label>
+                <input type="email" className='form-control' name='email' onChange={handleChange} />
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'>Password</label>
+                <input type="password" className='form-control' name='password' onChange={handleChange} />
+            </div>
+            <button className='btn btn-primary' onClick={submitForm}>Register</button>
+        </form>
     );
 }
 
