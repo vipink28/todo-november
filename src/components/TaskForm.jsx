ee066992b7fda6ba6f15a16215072a0e9536cf3f
vipink28 <vipinkalyan28@gmail.com>
2023-12-20 10:51:07 +0530

    task form

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TaskForm(props) {
+    return (
+        <div className='p-2'>
+            <h3 className='text-white'>Create Task</h3>
+        </div>
+    );
+}
+
+export default TaskForm;
--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import TaskForm from '../components/TaskForm';
 
 function CreateTask(props) {
     return (
-        <div>
-            Create Task
+        <div className='container-fluid h-100'>
+            <div className="row h-100">
+                <div className="col-lg-6 d-flex justify-content-center align-items-center h-100 bg-primary text-white flex-column">
+                    <TaskForm />
+                </div>
+
+                <div className="col-lg-6 d-flex justify-content-center align-items-center h-100 flex-column">
+
+                    <div className="card w-75">
+                        <div className="card-body">
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
